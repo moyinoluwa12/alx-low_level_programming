77e7a5b3b81c68fc1f2bf9ef91bf26d50e1db1db
moyinoluwa12 <moyingoke12@gmail.com>
2022-09-09 18:18:28 -0700

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
-int main()
+/**
+* main - Entry point
+*
+*Description:'Using put command to print an output'
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
 {
-puts("Programming is like building a multilingual puzzle\n");
-return 0;
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
 }
